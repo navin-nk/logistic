6371395eca75ce616ae548cd1e259420ec14135f
fulgid <navin@fulgid.in>
2020-05-18 23:39:30 +0530

    extra css animation and svg over effect added and mobile responsive bug fixed

--- js/custom.js
+++ js/custom.js
@@ -71,12 +71,12 @@ function myFunction(x) {
 
 //blog slide
 $('.owl-carousel1').owlCarousel({
-    loop:true,
+    loop:false,
     margin: 0,
     center: false,
     items: 4,
     dots:true,
-    autoplay: true,
+    autoplay: false,
     autoplayTimeout: 2500,
     smartSpeed: 450,
     responsive: {
@@ -146,4 +146,65 @@ $(document).ready(function(){
             });
         } // End if
     });
+});
+
+//menu scroll color change
+var lastId,
+    topMenu = $(".menu-content"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+    // Get container scroll position
+    var fromTop = $(this).scrollTop()+topMenuHeight;
+    // Get id of current scroll item
+    var cur = scrollItems.map(function(){
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length-1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        menuItems
+            .parent().removeClass("active1")
+            .end().filter("[href='#"+id+"']").parent().addClass("active1");
+    }
+});
+
+//Go to TOp
+var btn = $('#button');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
 });
